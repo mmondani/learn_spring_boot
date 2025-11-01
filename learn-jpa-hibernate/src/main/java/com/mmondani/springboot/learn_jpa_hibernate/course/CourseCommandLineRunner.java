968164e0ff2692fcceaf4037619fdcce5ff2f601
Mariano Mondani <mmondani89@gmail.com>
2025-11-01 20:25:06 -0300

    Se modifica el ejercicio anterior usando Spring Data JPA

--- learn-jpa-hibernate/src/main/java/com/mmondani/springboot/learn_jpa_hibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-hibernate/src/main/java/com/mmondani/springboot/learn_jpa_hibernate/course/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.mmondani.springboot.learn_jpa_hibernate.course;
 
 import com.mmondani.springboot.learn_jpa_hibernate.course.jdbc.CourseJdbcRepository;
 import com.mmondani.springboot.learn_jpa_hibernate.course.jpa.CourseJpaRepository;
+import com.mmondani.springboot.learn_jpa_hibernate.course.springDataJpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,17 +13,34 @@ public class CourseCommandLineRunner implements CommandLineRunner {
     //@Autowired
     //private CourseJdbcRepository repository;
 
+    //@Autowired
+    //private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS Now", "in28minutes"));
+        /*repository.insert(new Course(1, "Learn AWS Now", "in28minutes"));
         repository.insert(new Course(2, "Learn Azure", "in28minutes"));
         repository.insert(new Course(3, "Learn DevOps", "in28minutes"));
 
         repository.deleteById(2);
 
-        System.out.println(repository.findById(1));
+        System.out.println(repository.findById(1));*/
+
+        repository.save(new Course(1, "Learn AWS Now Data JPA", "in28minutes"));
+        repository.save(new Course(2, "Learn Azure Data JPA", "in28minutes"));
+        repository.save(new Course(3, "Learn DevOps Data JPA", "in28minutes"));
+
+        repository.deleteById(2l);
+
+        System.out.println(repository.findById(1l));
+
+        System.out.println(repository.findAll());
+        System.out.println(repository.count());
+
+        System.out.println(repository.findByAuthor("in28minutes"));
+        System.out.println(repository.findByAuthor(""));
     }
 }
--- learn-jpa-hibernate/src/main/java/com/mmondani/springboot/learn_jpa_hibernate/course/springDataJpa/CourseSpringDataJpaRepository.java
+++ learn-jpa-hibernate/src/main/java/com/mmondani/springboot/learn_jpa_hibernate/course/springDataJpa/CourseSpringDataJpaRepository.java
@@ -1,4 +1,10 @@
 package com.mmondani.springboot.learn_jpa_hibernate.course.springDataJpa;
 
-public interface CourseSpringDataJpaRepository {
+import com.mmondani.springboot.learn_jpa_hibernate.course.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+    List<Course> findByAuthor(String author);
 }
