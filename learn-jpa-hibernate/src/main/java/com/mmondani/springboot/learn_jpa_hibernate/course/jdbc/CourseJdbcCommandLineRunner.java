45fcdedae33c5c14ed5d81f3c3bec051073af78c
Mariano Mondani <mmondani89@gmail.com>
2025-11-01 19:30:55 -0300

    Commit incial del curso Master Spring Boot 3 & Spring Framework 6 with Java. Se incluye un ejercicio básico de Spring Boot y un ejercicio de JDBC/JPA

---
+++ learn-jpa-hibernate/src/main/java/com/mmondani/springboot/learn_jpa_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,24 @@
+package com.mmondani.springboot.learn_jpa_hibernate.course.jdbc;
+
+import com.mmondani.springboot.learn_jpa_hibernate.course.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+
+    @Autowired
+    private CourseJdbcRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.insert(new Course(1, "Learn AWS Now", "in28minutes"));
+        repository.insert(new Course(2, "Learn Azure", "in28minutes"));
+        repository.insert(new Course(3, "Learn DevOps", "in28minutes"));
+
+        repository.deleteById(2);
+
+        System.out.println(repository.findById(1));
+    }
+}
